e63af2d01a0f6459bd13f8085499010069b240c3
Martins Pereira <martins.silva@etec.sp.gov.br>
2021-12-13 23:23:34 -0300

    first commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+import incomeImg from '../../assets/icon-entradas.svg'
+import outcomeImg from '../../assets/icon-saidas.svg'
+import total from '../../assets/total.svg'
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={outcomeImg} alt="Saidas" />
+        </header>
+        <strong>- R$5000,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={total} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  )
+}
